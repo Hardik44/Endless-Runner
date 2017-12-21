6a6ef8a9feacdcaae6a70faf1308c6e43994180e
Hardik44 <jhardik4445@gmail.com>
2017-12-22 01:50:55 +0530

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+
+    public float speed;
+
+    private Vector3 dir;
+
+    public GameObject ps;
+
+    private bool isDead;
+
+    public GameObject resetBtn;
+
+    private int score=0;
+
+    public Text ScoreText,newHighScore;
+
+    public Animator gameOverAnim;
+
+    public Image background;
+
+    public Text[] ScoreTexts;
+
+    public LayerMask wathIsGround;
+
+    private bool isPlaying=false;
+
+    public Transform contactPoint;
+
+    private static Player instance;
+
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Player>();
+            }
+            
+            return instance;
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        isDead = false;
+        dir = Vector3.zero;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!IsGrounded()&& isPlaying)
+        {
+            isDead = true;
+
+            GameOver();
+
+
+            resetBtn.SetActive(true);
+            if (transform.childCount > 0)
+            {
+                transform.GetChild(0).transform.parent = null;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isDead)
+        {
+            isPlaying = true;
+
+            score++;
+            ScoreText.text = score.ToString();
+
+            if (dir == Vector3.forward)
+            {
+                dir = Vector3.left;
+            }
+            else
+            {
+                dir = Vector3.forward;
+            }
+        }
+
+
+
+        //for Touch!
+
+        //foreach (Touch touch in Input.touches)
+        //{
+        //    if (touch.phase == TouchPhase.Began && !isDead)
+        //    {
+        //        isPlaying = true;
+
+        //        score++;
+        //        ScoreText.text = score.ToString();
+
+        //        if (dir == Vector3.forward)
+        //        {
+        //            dir = Vector3.left;
+        //        }
+        //        else
+        //        {
+        //            dir = Vector3.forward;
+        //        }
+        //    }
+        //}
+
+
+
+        float amountTomove = speed * Time.deltaTime;
+
+        transform.Translate(dir * amountTomove);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            Instantiate(ps, transform.position, Quaternion.identity);
+            score+=3;
+            ScoreText.text = score.ToString();
+
+        }
+    }
+
+
+
+    private void GameOver()
+    {
+        gameOverAnim.SetTrigger("GameOver");
+
+        ScoreTexts[1].text = score.ToString();
+        
+        int bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        
+        if (score > bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            newHighScore.gameObject.SetActive(true);
+            background.color = new Color32(243, 65, 246, 255);
+
+            foreach (Text txt in ScoreTexts)
+            {
+                txt.color=Color.white;
+            }
+        }
+        
+        ScoreTexts[3].text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+        
+    }
+
+    private bool IsGrounded()
+    {
+        Collider[] Colliders = Physics.OverlapSphere(contactPoint.position, .5f, wathIsGround);
+
+        for(int i = 0; i < Colliders.Length; i++)
+        {
+            if (Colliders[i].gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void OnExit()
+    {
+            Application.Quit();
+    }
+    
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileManager : MonoBehaviour {
+
+
+    public GameObject[] TilePrefabs;
+
+    public GameObject currentTile;
+
+    private static TileManager instance;
+
+    private Stack<GameObject> leftTiles = new Stack<GameObject>();
+
+    private Stack<GameObject> topTiles = new Stack<GameObject>();
+
+    public static TileManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TileManager>();
+            }
+            return instance;
+        }
+
+      
+    }
+
+    public Stack<GameObject> LeftTiles
+    {
+        get
+        {
+            return leftTiles;
+        }
+
+        set
+        {
+            leftTiles = value;
+        }
+    }
+
+    public Stack<GameObject> TopTiles
+    {
+        get
+        {
+            return topTiles;
+        }
+
+        set
+        {
+            topTiles = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        CreateTiles(100);
+        for (int i = 0; i < 50; i++)
+        {
+            SpawnTile();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void CreateTiles (int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            LeftTiles.Push(Instantiate(TilePrefabs[0]));
+            TopTiles.Push(Instantiate(TilePrefabs[1]));
+            TopTiles.Peek().name = "TopTile";
+            TopTiles.Peek().SetActive(false);
+            LeftTiles.Peek().name = "LeftTile";
+            LeftTiles.Peek().SetActive(false);
+           
+        }
+    }
+    public void SpawnTile()
+    {
+
+        if(TopTiles.Count == 0 || LeftTiles.Count == 0)
+        {
+            CreateTiles(10);
+        }
+
+
+        int RandomIndex = Random.Range(0, 2);
+
+        if (RandomIndex == 0)
+        {
+            GameObject tmp = LeftTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(RandomIndex).position;
+            currentTile = tmp;
+        }
+        else if(RandomIndex==1)
+        {
+
+            GameObject tmp = TopTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(RandomIndex).position;
+            currentTile = tmp;
+
+        }
+
+        int spawnPickup = Random.Range(0, 10);
+
+        if (spawnPickup == 0)
+        {
+            currentTile.transform.GetChild(1).gameObject.SetActive(true);
+        }
+
+        currentTile =(GameObject) Instantiate(TilePrefabs[RandomIndex], currentTile.transform.GetChild(0).transform.GetChild(RandomIndex).position, Quaternion.identity);
+    }
+
+    public void ResetGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
